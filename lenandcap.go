36a6635378a7e1f27d4bc49d47b31a058065ef93
justtechdeveloper@gmail.com <justtechdeveloper@gmail.com>
2021-10-16 09:26:41 +0530

    Removed Unwanted Files

--- lenandcap.go
+++ lenandcap.go
@@ -8,9 +8,8 @@ func main() {
 	s := make([]int, 0, 2)
 	for i := 0; i < 5; i++ {
 		s = append(s, i)
-		fmt.Printf("cap %v, len %v, %p\n", cap(s), len(s), s)
+		fmt.Printf("cap %v, len %v, %p\n", len(s), s)
 	}
 }
 
 // len return the len of the array
-// cap will return
